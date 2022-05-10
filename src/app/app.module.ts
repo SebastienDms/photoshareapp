80517c12f87f3f6189a92019d0184477f36062a1
Sebastien Damsin <sebastien.damsin@inforius.be>
2022-05-10 10:30:12 +0200

    PhotoShareApp - init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PhotoShareComponent } from './photo-share/photo-share.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoShareComponent
   ],
   imports: [
     BrowserModule
