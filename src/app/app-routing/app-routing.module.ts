53be0708a0b86cee1ce9f1db5d5af152207af229
Sebastien Damsin <sebastien.damsin@inforius.be>
2022-05-27 16:50:56 +0200

    Mise à jour gestion du routing
    Update routing

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,10 +4,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from '../shared/component/not-found/not-found.component';
 import { WelcomeComponent } from '../shared/component/welcome/welcome.component';
 import { PhotosMainComponent } from '../features/photoShare/photos-main/photos-main.component';
+import { PhotoShareListeComponent } from '../features/photoShare/photo-share-liste/photo-share-liste.component';
+import { PhotoShareComponent } from '../features/photoShare/photo-share/photo-share.component';
+import { InternCalendarComponent } from '../features/intern-calendar/intern-calendar.component';
 
 const routes: Routes = [
   {
-    path: '', component: WelcomeComponent /*, pathMatch:'full', redirectTo: '/'*/
+    path: 'home', component: WelcomeComponent
+  },
+  {
+    path: 'photos', component: PhotosMainComponent
+  },
+  {
+    path: 'photos/list', component: PhotoShareListeComponent
+  },
+  {
+    path: 'photo/:id', component: PhotoShareComponent
+  },
+  {
+    path: 'calendrier', component: InternCalendarComponent
+  },
+  {
+    path: '', pathMatch:'full', redirectTo: '/home'
   },
   {
     path: '**', component: NotFoundComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PhotoShareComponent } from './features/photoShare/photo-share/photo-share.component';
-import * as fr_be from "@angular/common/locales/fr-BE";
 import { PhotoShareListeComponent } from './features/photoShare/photo-share-liste/photo-share-liste.component';
+import { PhotosMainComponent } from './features/photoShare/photos-main/photos-main.component';
+
+import * as fr_be from "@angular/common/locales/fr-BE";
 import { HeaderComponent } from './shared/component/header/header.component';
 import { FooterComponent } from './shared/component/footer/footer.component';
 
@@ -13,10 +15,8 @@ import { CalendarModule, DateAdapter } from "angular-calendar";
 import { adapterFactory } from "angular-calendar/date-adapters/date-fns";
 import { InternCalendarComponent } from './features/intern-calendar/intern-calendar.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { PhotoShareModule } from './features/photoShare/photo-share.module';
 import { NotFoundComponent } from './shared/component/not-found/not-found.component';
 import { WelcomeComponent } from './shared/component/welcome/welcome.component';
-import { PhotosMainComponent } from './features/photoShare/photos-main/photos-main.component';
 
 @NgModule({
   declarations: [
@@ -25,11 +25,13 @@ import { PhotosMainComponent } from './features/photoShare/photos-main/photos-ma
     FooterComponent,
     WelcomeComponent,
     NotFoundComponent,
+    PhotosMainComponent,
+    PhotoShareComponent,
+    PhotoShareListeComponent,
     InternCalendarComponent,
   ],
   imports: [
     BrowserModule,
-    PhotoShareModule,
     AppRoutingModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
